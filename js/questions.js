cea915baa2dc4adecdbd5a92c520efa418d4e6cc
ANSHCHAWLA119 <95087629+ANSHCHAWLA119@users.noreply.github.com>
2022-05-20 21:00:05 +0530

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Who Is The Prime Minister Of India?",
+    answer: "Narendra Damodardas Modi",
+    options: [
+      "Arvind Kejriwal",
+      "Narendra Damodardas Modi",
+      "Rahul Gandhi",
+      "Mukesh Ambani"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Who won IPL in 2021?",
+    answer: "Chennai Super Kings",
+    options: [
+      "Delhi Capitals",
+      "Royal Challenger's Banglore",
+      "Chennai Super Kings",
+      "Rajasthan Royals"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Who Is The National Animal Of India?",
+    answer: "Tiger",
+    options: [
+      "Tiger",
+      "Crocodile",
+      "hippopotamus",
+      "Giraffe"
+    ]
+  },
+    {
+    numb: 4,
+    question: "When Is Republic Day Celebrated In India?",
+    answer: "26 January",
+    options: [
+      "17 April",
+      "26 January",
+      "5 May",
+      "7 July"
+    ]
+  },
+    {
+    numb: 5,
+    question: "when is environment day celebrated?",
+    answer: "5 June",
+    options: [
+      "22 April",
+      "17 January",
+      "26 August",
+      "5 June"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
